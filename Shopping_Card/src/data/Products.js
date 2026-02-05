73d2ea38fb4777bacf9c65a69d7538842543f410
aarivazhagi17 <aarivazhagi17@gmail.com>
2026-02-05 13:30:27 +0530

    new one

---
+++ Shopping_Card/src/data/Products.js
@@ -0,0 +1,28 @@
+import Iphone15 from '../assets/product/Iphone15.jpg'
+import Acer from '../assets/product/Acer.jpg'
+import Ovemb from '../assets/product/Ovemb.jpg'
+import Nikon from '../assets/product/Nikon.jpg'
+import OnePlus11 from '../assets/product/Oneplus11.jpg'
+import Goboult from '../assets/product/Goboult.jpg'
+import hp from '../assets/product/hp.jpg'
+import Sonatta from '../assets/product/Sonatta.jpg'
+import PocoM3 from '../assets/product/PocoM3.jpg'
+import Zhiyun from '../assets/product/Zhiyun.jpg'
+import OnePlus from '../assets/product/Oneplus.jpg'
+import Lenovo from '../assets/product/Lenovo.jpg'
+const Products = [
+  { id: 1, name: "Iphone15", price: 16.777, image: Iphone15},
+  { id: 2, name: "Acer", price: 39.990, image: Acer},
+  { id: 3, name: "Ovemb", price: 347 , image: Ovemb},
+  { id: 4, name: "Nikon", price: 65.999 ,image: Nikon},
+  { id: 5, name: "OnePlus11", price: 44.999 ,image: OnePlus11},
+  { id: 6, name: "Goboult", price: 859 ,image: Goboult},
+  { id: 7, name: "hp", price: 26.809 ,image: hp},
+  { id: 8, name: "Sonatta", price: 2.795 ,image: Sonatta},
+  { id: 9, name: "PocoM3", price: 13.999 ,image: PocoM3},
+  { id: 10, name: "Zhiyun", price: 53.999 ,image: Zhiyun},
+  { id: 11, name: "OnePlus", price: 1.499 ,image: OnePlus},
+  { id: 12, name: "Lenovo", price: 32.999 ,image: Lenovo},
+];
+
+export default Products;
